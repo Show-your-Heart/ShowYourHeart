92045267d46eaa2587805945c17589a24f3f7124
Silvia <silvia@dabne.com>
2025-05-06 10:23:18 +0200

    Change organization model

--- src/apps/organizations/models.py
+++ src/apps/organizations/models.py
@@ -5,7 +5,17 @@ from project.models import BaseModel
 
 
 class Organization(BaseModel):
-    STATUS = {"0": "Pending", "1": "Accepted", "2": "Rejected"}
+    class Status(models.IntegerChoices):
+        PENDING = (
+            0,
+            "Pending",
+        )
+        ACCEPTED = (
+            1,
+            "Accepted",
+        )
+        REJECTED = 2, "Rejected"
+
     name = models.CharField(_("name"), max_length=50)
     vat_number = models.CharField(_("vat number"), max_length=20)
     contact = models.ForeignKey("users.User", on_delete=models.CASCADE)
@@ -13,7 +23,9 @@ class Organization(BaseModel):
     country = models.CharField(_("country"), max_length=50)
     region = models.CharField(_("region"), max_length=50)
     city = models.CharField(_("city"), max_length=50)
-    status = models.CharField(max_length=1, choices=STATUS)
+    status = models.PositiveSmallIntegerField(
+        choices=Status.choices, default=Status.PENDING
+    )
     primary_legal_structure = models.ForeignKey(
         "settings.LegalStructure",
         on_delete=models.CASCADE,
