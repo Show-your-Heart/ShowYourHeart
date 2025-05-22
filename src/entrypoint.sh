e7ce1db6414ec3c34e5e458042c50c069c7e051d
konykon <kon.tsagari@coopdevs.org>
2025-05-22 18:04:40 +0200

    Execute migrate and collectstatic

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,6 +8,8 @@ start_gunicorn() {
 
     # Start Gunicorn
     echo "Starting Gunicorn..."
+    python manage.py migrate
+    echo yes | python manage.py collectstatic
     gunicorn --bind 0.0.0.0:8000 --reload --reload-engine=poll $extra_files project.wsgi:application --threads=10
 
 }
