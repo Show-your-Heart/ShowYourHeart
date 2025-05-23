9d038ff08ebb433b516d048a7cdf1a6f2a98dda5
Silvia <silvia@dabne.com>
2025-05-23 10:35:24 +0200

    Set topic translations on new tab

--- src/apps/methods/admin.py
+++ src/apps/methods/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
 from unfold.admin import ModelAdmin
@@ -7,6 +8,36 @@ from .models import Indicator, Topic
 
 class TopicAdmin(ModelAdmin, TranslationAdmin):
     list_display = ("name", "description", "parent")
+
+    def get_fieldsets(self, request, obj=None):
+        # Get all configured language codes except 'en'
+        other_langs = [lang[0] for lang in settings.LANGUAGES if lang[0] != "en"]
+        translatable_fields = [
+            "name",
+            "description",
+        ]
+
+        main_fields = ["name_en", "description_en", "parent"]
+        other_fields = [
+            f"{field}_{lang}" for field in translatable_fields for lang in other_langs
+        ]
+
+        return [
+            (
+                "Add/Edit Topic",
+                {
+                    "fields": main_fields,
+                    "classes": ("tab",),
+                },
+            ),
+            (
+                "Translations",
+                {
+                    "fields": other_fields,
+                    "classes": ("tab",),
+                },
+            ),
+        ]
 
 
 class IndicatorAdmin(ModelAdmin, TranslationAdmin):
