561626be1ebd2a849180e78f8d9aae0f1ff89d86
Silvia <silvia@dabne.com>
2025-05-26 10:00:02 +0200

    Fix Unfold errors

--- src/project/apps.py
+++ src/project/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "project"
+
+    def ready(self):
+        # Monkey-patch to adapt post_office models to Unfold version 0.57
+        from post_office.admin import AttachmentInline, EmailTemplateInline
+
+        EmailTemplateInline.ordering_field = ("",)
+        AttachmentInline.ordering_field = ("",)
