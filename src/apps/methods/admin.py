9d3a032f0ce3a624034bc00583539e874ec2bedd
silviaDabne <silvia@dabne.net>
2025-05-30 15:13:06 +0200

    Standardize how model log info is displayed (#77)
    Co-authored-by: Silvia <silvia@dabne.com>

--- src/apps/methods/admin.py
+++ src/apps/methods/admin.py
@@ -1,13 +1,41 @@
 from django.conf import settings
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
-from unfold.admin import ModelAdmin
+
+from project.admin import ModelAdmin
 
 from .models import Indicator, Topic
 
 
 class TopicAdmin(ModelAdmin, TranslationAdmin):
-    list_display = ("name", "description", "parent")
+    list_display = (
+        "name",
+        "description",
+        "parent",
+    )
+
+    fieldsets = [
+        (
+            (""),
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "parent",
+                )
+            },
+        ),
+        (
+            ("Log"),
+            {
+                "fields": (
+                    "created_by",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    ]
 
     def get_fieldsets(self, request, obj=None):
         # Get all configured language codes except 'en'
@@ -53,6 +81,39 @@ class IndicatorAdmin(ModelAdmin, TranslationAdmin):
         "condition": "is_direct_indicator == true",
         "formula": "is_direct_indicator == false",
     }
+    common_fieldsets = [
+        (
+            ("Log"),
+            {
+                "fields": (
+                    "created_by",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    ]
+
+    def get_fieldsets(self, request, obj=None):
+        # Do not display "log fields" twice, display them only on a "Log" section
+        log_fields = [
+            "created_by",
+            "created_at",
+            "updated_at",
+        ]
+        default_fields = super().get_fieldsets(request, obj)
+
+        filtered_default_fields = [
+            (
+                None,
+                {
+                    "fields": [
+                        f for f in default_fields[0][1]["fields"] if f not in log_fields
+                    ]
+                },
+            )
+        ]
+        return filtered_default_fields + self.common_fieldsets
 
 
 admin.site.register(Topic, TopicAdmin)
--- src/apps/organizations/admin.py
+++ src/apps/organizations/admin.py
@@ -11,3 +11,36 @@ class OrganizationAdmin(ModelAdmin):
         "name",
         "status",
     )
+    common_fieldsets = [
+        (
+            ("Log"),
+            {
+                "fields": (
+                    "created_by",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    ]
+
+    def get_fieldsets(self, request, obj=None):
+        # Do not display "log fields" twice, display them only on a "Log" section
+        log_fields = [
+            "created_by",
+            "created_at",
+            "updated_at",
+        ]
+        default_fields = super().get_fieldsets(request, obj)
+
+        filtered_default_fields = [
+            (
+                None,
+                {
+                    "fields": [
+                        f for f in default_fields[0][1]["fields"] if f not in log_fields
+                    ]
+                },
+            )
+        ]
+        return filtered_default_fields + self.common_fieldsets
