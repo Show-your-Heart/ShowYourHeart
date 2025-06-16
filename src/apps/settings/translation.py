794a4a2bc518aaa35e49e575c0f62f2648d182f7
silviaDabne <silvia@dabne.net>
2025-06-16 15:55:17 +0200

    Add sectors

--- src/apps/settings/translation.py
+++ src/apps/settings/translation.py
@@ -1,8 +1,12 @@
 from modeltranslation.translator import TranslationOptions, register
 
-from .models import LegalStructure
+from .models import LegalStructure, Sector
 
 
 @register(LegalStructure)
 class LegalStructureTranslationOptions(TranslationOptions):
+    fields = ("name",)
+
+@register(Sector)
+class SectorTranslationOptions(TranslationOptions):
     fields = ("name",)
