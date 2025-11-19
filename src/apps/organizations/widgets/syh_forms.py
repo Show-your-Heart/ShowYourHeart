4cefd1bf3098793102ecd7e13459c7ec3e3638d2
konykon <kon.tsagari@coopdevs.org>
2025-11-19 13:04:53 +0100

    Create date input widget

--- src/apps/organizations/widgets/syh_forms.py
+++ src/apps/organizations/widgets/syh_forms.py
@@ -3,3 +3,8 @@ from django import forms
 
 class FileInput(forms.ClearableFileInput):
     template_name = "organizations/components/file.html"
+
+
+class DateInput(forms.DateInput):
+    template_name = "organizations/components/date_input.html"
+    input_type = "date"
