0d4c6ef00811299263257eabca285ebf13b9c5ce
Luis H. Porras <luishporras@amoved.es>
2026-02-19 14:56:03 +0100

    fix: method selection field in register form

--- src/apps/organizations/widgets/syh_forms.py
+++ src/apps/organizations/widgets/syh_forms.py
@@ -8,3 +8,9 @@ class FileInput(forms.ClearableFileInput):
 class DateInput(forms.DateInput):
     template_name = "organizations/components/date_input.html"
     input_type = "date"
+
+
+class CheckboxSelectMultiple(forms.SelectMultiple):
+    template_name = "organizations/components/checkbox_select.html"
+    option_template_name = "organizations/components/checkbox.html"
+    use_fieldset = False
