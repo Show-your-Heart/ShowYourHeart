474bba8ab3e7a3d9a40b7ac8ac667915cb495cf7
konykon <kon.tsagari@coopdevs.org>
2026-05-07 14:25:29 +0200

    Add static url to project

--- src/project/urls.py
+++ src/project/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import include, path
@@ -42,3 +44,5 @@ urlpatterns += i18n_patterns(
     # Do not translate the methods url because is used on the html templates
     path("methods/", include("apps.methods.urls", namespace="methods")),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
