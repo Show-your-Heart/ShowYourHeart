171b25fc39559e468815e329ea2fae025a5b7729
konykon <kon.tsagari@coopdevs.org>
2026-07-23 18:44:00 +0200

    feat: Add smtp from_email helper function

--- src/project/utils/smtp_utils.py
+++ src/project/utils/smtp_utils.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.conf import settings
 
 
 def get_smtp_for_user(user):
@@ -13,3 +14,11 @@ def get_smtp_for_user(user):
     smtp = SMTPServer.objects.filter(network__in=user_networks).first()
 
     return smtp
+
+
+def get_from_email(user):
+    smtp = get_smtp_for_user(user)
+
+    from_email = smtp.from_email if smtp else settings.DEFAULT_FROM_EMAIL
+
+    return from_email
